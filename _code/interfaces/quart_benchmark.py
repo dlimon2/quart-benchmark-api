150e60665e7c186575be0f4fb8e3d226530ff79b
Daniel Limón <dlimon.munoz@gmail.com>
2024-12-01 06:56:02 -0600

    add:ComplexList custom type defined on QuartBenchmark class. It is used on QuartBenchmark.json_serialization() method

--- _code/interfaces/quart_benchmark.py
+++ _code/interfaces/quart_benchmark.py
@@ -1,5 +1,9 @@
 
 import asyncio, json
+from typing import List, Dict, Union
+
+ComplexList = List[Dict[str,
+                        Union[int, str, float]]]
 
 class QuartBenchmark:
     """This class performs a variety of intensive common tasks on HTTP APIs.
@@ -8,10 +12,11 @@ class QuartBenchmark:
     """
 
     async def json_serialization(self) -> str:
-        """Generate a list with nested dicts that holds numbers, strings and booleans,
+        """Generate a list with nested dicts that holds int, float and strings,
         then serialize it usin stdlib json module and returns that json string
         """
-        data: list[dict] = [{'id': i,
+        data: ComplexList = [{
+                             'id': i,
                              'name': f'Name{i}',
                              'value': i*3.14} for i in range(50_000)]
         
